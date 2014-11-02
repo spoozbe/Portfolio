4818a58eb50cbec949854456af6bbd49d6f37716
Kevin Venables <venablesk@gmail.com>
2014-11-01 21:49:25 -0700

    Euler 3. C# in da house.

---
+++ Euler/Euler3/Euler3/Euler3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+ */
+
+namespace Euler3
+{
+    class Program
+    {
+        static long number = 600851475143;
+        static long numm = number;
+        static long largest = 0;
+        static void Main(string[] args)
+        {
+            for (int i = 2; i * i <= numm; i++) //something we only need to check up to the square root
+            {
+                if (numm % i == 0)
+                {
+                    numm = numm / i;
+                }
+            }
+
+            Console.WriteLine("The largest prime of 600851475143 is: " + numm);
+        }
+
+    }
+}
